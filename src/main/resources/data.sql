ea09078ba321d7f9837799d7a95fc0f4ab5831f5
Wojtek Wilk <wilk.wojtek@gmail.com>
2016-04-09 16:16:41 +0200

    initial data changed

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,20 +1,8 @@
-insert into card(id, question, answer) values(2, 'cat', 'kot');
+insert into card(id, question, answer) values(2, 'Color of the sky', 'blue');
 insert into repetition(id, card_id, date, status) values(1, 2, '2016-04-08 00:00:00.0', 'PLANNED');
 
-insert into card(id, question, answer) values(1, 'bat', 'nietoperz');
+insert into card(id, question, answer) values(1, 'Biggest city in the world', 'Tokyo');
 insert into repetition(id, card_id, date, status) values(2, 1, '2016-04-08 00:00:00.0', 'PLANNED');
 
-insert into card(id, question, answer) values(3, 'jump', 'skok');
+insert into card(id, question, answer) values(3, 'Biggest ocean', 'Pacific');
 insert into repetition(id, card_id, date, status) values(3, 3, '2016-04-08 00:00:00.0', 'PLANNED');
-
-insert into card(id, question, answer) values(4, 'music', 'muzyka');
-insert into repetition(id, card_id, date, status) values(4, 4, '2016-04-08 00:00:00.0', 'PLANNED');
-
-insert into card(id, question, answer) values(5, 'silence', 'cisza');
-insert into repetition(id, card_id, date, status) values(5, 5, '2016-04-08 00:00:00.0', 'PLANNED');
-
-insert into card(id, question, answer) values(6, 'face', 'twarz');
-insert into repetition(id, card_id, date, status) values(6, 6, '2016-04-08 00:00:00.0', 'PLANNED');
-
-insert into card(id, question, answer) values(7, 'twarz', 'face');
-insert into repetition(id, card_id, date, status) values(7, 7, '2016-04-08 00:00:00.0', 'PLANNED');
